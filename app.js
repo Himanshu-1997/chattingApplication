80076eac8e9f70546a69d0cadb73934d38dd4e45
Himanshu Kumar <himanshu28051997@gmail.com>
2018-07-15 19:04:54 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,98 @@
+var express = require('express'),
+		app = express(),
+		server = require('http').createServer(app),
+		io = require('socket.io').listen(server),
+		mongoose = require('mongoose'),
+		users={};
+		
+const port=process.env.PORT||3000;
+//server started
+/*app.listen(3000, function(){
+  console.log('Server started on port 3000...');
+});*/
+
+mongoose.connect('mongodb://chat', function(err){
+	if(err){
+		console.log('Error!!!!!!!!!!!!!!!!!!!!!!!!!!');
+	}
+	else{
+		console.log('connected to the mongodb database!!');
+	}
+});
+
+
+server.listen(port);
+
+var chatSchema = mongoose.Schema({
+	nick:String,
+	msg:String,
+	created:{type: Date,default: Date.now}
+});
+
+var Chat = mongoose.model('Message',chatSchema);
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection',function(socket){
+	var query = Chat.find({});
+	query.sort('-created').limit(8).exec(function(err,docs){
+		if(err){
+			throw err;
+		}
+		socket.emit('load old messages',docs);
+	});
+	
+	socket.on('new user',function(data,callback){
+		if(data in users){
+			callback(false);
+		}
+		else{
+			callback(true);
+			socket.nickname = data;
+			users[socket.nickname]=socket;
+			updateNickNames();
+		}
+	});
+	
+	function updateNickNames(){
+		io.sockets.emit('usernames',Object.keys(users));
+	}
+	socket.on('send message',function(data,callback){
+		var msg=data.trim();
+		if(msg.substr(0,3)=== '/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if(ind!==-1){
+				var name = msg.substring(0,ind);
+				var msg = msg.substring(ind+1);
+				if(name in users){
+					users[name].emit('whisper',{msg:msg, nick:socket.nickname});
+					console.log('whisper!');
+				}
+				else{
+					callback('Error! enter a valid user!!');
+				}
+			}
+			else{
+				callback('Error! please enter a message for your whisper!');
+			}
+		}
+		else{
+			var newMsg = new Chat({msg:msg, nick:socket.nickname});
+			newMsg.save(function(err){
+				if(err)
+					throw err;
+					io.sockets.emit('new message',{msg:msg, nick:socket.nickname});
+				});
+		}
+	});
+	
+	socket.on('disconnect',function(data){
+		if(!socket.nickname) return;
+		delete users[socket.nickname];
+		updateNickNames();
+	});
+	
+});
